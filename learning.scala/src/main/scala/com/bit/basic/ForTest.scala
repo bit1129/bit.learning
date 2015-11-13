2ab67f78d2abfc4b084e854e7eb7ddb80619eb89
bit1129 <bit1129@163.com>
2015-11-13 14:20:35 +0800

    val can't be used in for loop variable

--- learning.scala/src/main/scala/com/bit/basic/ForTest.scala
+++ learning.scala/src/main/scala/com/bit/basic/ForTest.scala
@@ -9,10 +9,17 @@ class ForTest {
     val arr = Array(1, 2, 3)
 
     /**
-     for (val x <- arr)有错
-     */
-    for ( x <- arr) {
+    for (val x <- arr)有错
+      */
+    for (x <- arr) {
       print(x + " ")
+    }
+
+    /**
+     * val y 能够使用，不过deprecated
+     */
+    for (x <- arr; val y = x) {
+      print(y + " ")
     }
   }
 }
