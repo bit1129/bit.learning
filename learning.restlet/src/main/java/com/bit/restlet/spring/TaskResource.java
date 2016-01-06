5f3cfdbd2cd9612b46dc40d27526e159c50dd48e
yuzhitao <yuzhitao@jd.com>
2016-01-06 19:17:37 +0800

    restlet framework to process param in URL

--- learning.restlet/src/main/java/com/bit/restlet/spring/TaskResource.java
+++ learning.restlet/src/main/java/com/bit/restlet/spring/TaskResource.java
@@ -1,68 +1,13 @@
 package com.bit.restlet.spring;
 
-import com.bit.restlet.standalone.InMemoryData;
-import com.bit.restlet.standalone.ResponseObject;
-import com.bit.restlet.standalone.Task;
-import org.json.JSONObject;
 import org.restlet.data.Form;
-import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
-import org.restlet.resource.Delete;
 import org.restlet.resource.Get;
-import org.restlet.resource.Put;
 import org.restlet.resource.ServerResource;
 
 import java.util.Set;
 
 public class TaskResource extends ServerResource {
-
-    /**
-     * 通过JsonRepresentation获取请求参数
-     *
-     * @param representation
-     * @return
-     */
-    @Delete
-    public Representation delete(JsonRepresentation representation) {
-        JSONObject jo = representation.getJsonObject();
-        int id = jo.getInt("id");
-        Task task = new Task();
-        task.setId(id);
-        boolean b = InMemoryData.data.delete(task);
-        if (b) {
-            return new JsonRepresentation(ResponseObject.SUCC);
-        } else {
-            return new JsonRepresentation(ResponseObject.FAIL);
-        }
-
-    }
-
-
-    /**
-     * 获取客户端发送来的header值,restlet将所有的headers放到org.restlet.http.headers这个attribute中了
-     *
-     * @param representation
-     * @return
-     */
-    @Put
-    public Representation put(Representation representation) {
-        Form headers = (Form) getRequest().getAttributes().get("org.restlet.http.headers");
-        //names是header name么？
-        Set<String> names = headers.getNames();
-        for (String name : names) {
-            System.out.println(name);
-        }
-
-        //header-user-name是自定义的header
-        String value = headers.getValues("header-user-name");
-        System.out.println("value is: " + value);
-
-        //query-param-user-name是请求参数，类似?query-param-user-name=123，这里得不到值，因为headers是封装的headers信息
-        value = headers.getValues("query-param-user-name");
-        System.out.println("value is: " + value);
-
-        return null;
-    }
 
     @Get
     public Representation get(Representation representation) {
@@ -72,6 +17,13 @@ public class TaskResource extends ServerResource {
         for (String name : names) {
             System.out.println(name);
         }
+
+        // /tasks/1/2?q1=1&q2=2
+        String taskId = (String) getRequest().getAttributes().get("taskId");
+        System.out.println(taskId);
+
+        String subTaskId = (String) getRequest().getAttributes().get("subTaskId");
+        System.out.println(subTaskId);
 
         //?q1=123&q2=456
         String value = q.getValues("q1");
