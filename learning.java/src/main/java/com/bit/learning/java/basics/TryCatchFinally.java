2eb1ec5b6760119f1e9668f5b39f463f912b4b73
yuzhitao <yuzhitao@jd.com>
2016-03-03 17:39:06 +0800

    TryCatchFinallyTest

---
+++ learning.java/src/main/java/com/bit/learning/java/basics/TryCatchFinally.java
@@ -0,0 +1,25 @@
+package com.bit.learning.java.basics;
+
+/**
+ * Created by yuzt on 16-3-3.
+ */
+public class TryCatchFinally {
+    public static void main(String[] args) {
+        Integer i = 1;
+        System.out.println(testTryCatchFinally(i));
+
+        i = null;
+        System.out.println(testTryCatchFinally(i));
+    }
+
+    public static Integer testTryCatchFinally(Integer i) {
+        try {
+            int a = i + 10;
+            return a;
+        } catch (Exception e) {
+            return -1;
+        } finally {
+
+        }
+    }
+}
