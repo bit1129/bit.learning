97791ab497c0931e680f2570a282213dc132423b
yuzhitao <yuzhitao@jd.com>
2016-03-25 17:53:58 +0800

    Drill docs

---
+++ learning.scala/src/main/scala/com/bit/basic/RangeTest.scala
@@ -0,0 +1,12 @@
+package com.bit.basic
+
+/**
+  * Created by yuzt on 16-3-25.
+  */
+object RangeTest {
+  def main(args : Array[String]): Unit = {
+    val range = 0.until(1)
+    println(range)
+  }
+
+}
