1e6b65255645dd4b8eabb9cfe89226eb47898d9f
yuzhitao <yuzhitao@jd.com>
2016-04-08 20:48:22 +0800

    Java web

--- learning.web/src/main/webapp/scripts/editableTable.js
+++ learning.web/src/main/webapp/scripts/editableTable.js
@@ -1,10 +1,22 @@
 $(function () {
     //页面加载完成，开始加载数据，渲染页面
-
     function _fireRequestAndRenderUI() {
+        $.ajax({
+            type: "get",
+            url: "persons",
+            success: function (data) {
+                alert(data);
+            },
+            error: function (error) {
+                alert("Error: " + error.responseText);
+            }
 
+        })
     }
+
     function _doRenderUI() {
 
     }
+
+    _fireRequestAndRenderUI();
 });
